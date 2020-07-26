09d175a65612f4053ad7c6d761fc4403546d2503
allen <onlychuan95@gmail.com>
2020-07-26 15:29:50 +1000

    version 1

---
+++ MYSURFtv/src/app/channel/channel.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-channel',
+  templateUrl: './channel.page.html',
+  styleUrls: ['./channel.page.scss'],
+})
+export class ChannelPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
